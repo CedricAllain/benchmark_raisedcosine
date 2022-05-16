fix loss computation

benchmark/compare_nll.py
@@ -9,7 +9,7 @@ from joblib import Memory, Parallel, delayed
 from raised_torch.simu_pp import simu
 from raised_torch.model import Model
 from raised_torch.solver import initialize, compute_loss
-from raised_torch.utils.utils import check_tensor
+from raised_torch.utils.utils import check_tensor, kernel_intensity
 
 from trunc_norm_kernel.model import TruncNormKernel, Intensity
 from trunc_norm_kernel.metric import negative_log_likelihood
@@ -28,14 +28,15 @@ if n_drivers == 1:
     baseline = 2.
     alpha = [1.]
     # alpha = [0]
-    m = [0.4]
+    # m = [0.5]
+    m = [0.9]
     sigma = [0.2]
     isi = [1]
 elif n_drivers == 2:
     baseline = 1
     alpha = [1., 1]
-    m = [0.4, 0.6]
-    sigma = [0.1, 0.05]
+    m = [0.5, 0.9]
+    sigma = [0.1, 0.2]
     isi = [1, 1]
 
 lower, upper = 0, 1
@@ -67,24 +68,26 @@ def procedure(T, L, p_task=0.6, verbose=False):
         integ += this_alpha * len(this_driver_tt)
 
     # %% compute nll with DriPP without discretization
-    kernel = [TruncNormKernel(lower, upper, use_dis=False)
-              for _ in range(n_drivers)]
-    intensity = Intensity(kernel=kernel, driver_tt=driver_tt, acti_tt=acti_tt)
-    intensity.update(baseline, alpha, m, sigma)
-    nll_dripp_cont = negative_log_likelihood(intensity, T)
-    log_intensity_cont = np.log(intensity(intensity.acti_tt,
-                                     driver_delays=intensity.driver_delays)).sum()
+    kernel_cont = [TruncNormKernel(lower, upper, use_dis=False)
+                   for _ in range(n_drivers)]
+    intensity_cont = Intensity(
+        kernel=kernel_cont, driver_tt=driver_tt, acti_tt=acti_tt)
+    intensity_cont.update(baseline, alpha, m, sigma)
+    nll_dripp_cont = negative_log_likelihood(intensity_cont, T)
+    log_intensity_cont = np.log(intensity_cont(intensity_cont.acti_tt,
+                                               driver_delays=intensity_cont.driver_delays)).sum()
 
     # compute nll with DriPP with discretization
-    kernel = [TruncNormKernel(lower, upper, sfreq=L, use_dis=True)
-              for _ in range(n_drivers)]
-    intensity = Intensity(kernel=kernel, driver_tt=driver_tt, acti_tt=acti_tt)
-    intensity.update(baseline, alpha, m, sigma)
-    nll_dripp_dis = negative_log_likelihood(intensity, T)
-    log_intensity_dis = np.log(intensity(intensity.acti_tt,
-                                     driver_delays=intensity.driver_delays)).sum()
+    kernel_dis = [TruncNormKernel(lower, upper, sfreq=L, use_dis=True)
+                  for _ in range(n_drivers)]
+    intensity_dis = Intensity(
+        kernel=kernel_dis, driver_tt=driver_tt, acti_tt=acti_tt)
+    intensity_dis.update(baseline, alpha, m, sigma)
+    nll_dripp_dis = negative_log_likelihood(intensity_dis, T)
+    log_intensity_dis = np.log(intensity_dis(intensity_dis.acti_tt,
+                                             driver_delays=intensity_dis.driver_delays)).sum()
 
-    # %% compute nll with torch
+    # compute nll with torch
     driver = check_tensor(driver).to(device)
     acti = check_tensor(acti).to(device).to(torch.bool)
 
@@ -93,50 +96,77 @@ def procedure(T, L, p_task=0.6, verbose=False):
                   lower=lower, upper=upper)
     # model = model.to(device)
     intensity_torch = model(driver)
+    nll_torch = compute_loss(
+        model.loss_name, intensity_torch, acti, model.dt).item()
     if verbose:
-        t_max = 10
+        t_max = 5
         plt.plot(t[:t_max*L], intensity_torch.detach().numpy()[:t_max*L])
         plt.stem(t[:t_max*L], acti[:t_max*L])
         plt.title("Intensity")
         plt.show()
-    nll_torch = compute_loss(
-        model.loss_name, intensity_torch, acti, model.dt).item()
+
+    # compute nll with torch based on dripp kernel
+    kernels_ = check_tensor([this_kernel(kernel_support.numpy())
+                             for this_kernel in kernel_dis])
+    intensity_mix = kernel_intensity(baseline, driver, kernels_, L)
+    nll_mix = compute_loss(
+        model.loss_name, intensity_mix, acti, model.dt).item()
+
     # compute integral estimation
     if verbose:
         print(f'L={L}, T={T}, p_task={p_task}')
         print("integ =", integ)
-        print("integ_est =", intensity_torch.sum()*dt)
+        print("integ_est =", (intensity_torch.sum()*dt).item())
+        print("integ_mix =", (intensity_mix.sum()*dt).item())
         print("log intensity at acti cont =", log_intensity_cont)
         print("log intensity at acti dis =", log_intensity_dis)
         print("torch log intensity at acti =",
-              torch.log(intensity_torch[acti]).sum())
-        print(f'nll_dripp_dis={nll_dripp_dis}')
-        print(f'nll_torch={nll_torch}')
+              torch.log(intensity_torch[acti]).sum().item())
+        print("mix log intensity at acti =",
+              torch.log(intensity_mix[acti]).sum().item())
+        print(f'nll_dripp_cont = {nll_dripp_cont}')
+        print(f'nll_dripp_dis = {nll_dripp_dis}')
+        print(f'nll_torch = {nll_torch}')
+        print(f'nll_mix = {nll_mix}')
 
     dict_res = dict(dripp_cont=nll_dripp_cont,
                     dripp_dis=nll_dripp_dis,
                     torch=nll_torch,
+                    mix=nll_mix,
                     T=T, L=L, p_task=p_task)
     return dict_res
 
 
-_ = procedure(T=10_000, L=100, p_task=0.6, verbose=True)
+def plot_losses(df_loss, methods, param_list, param, diff=True, xlogscale=False):
+    for method in methods:
+        if diff:
+            yy = df_loss[method] - df_loss['dripp_cont']
+        else:
+            yy = df_loss[method]
+        plt.plot(df_loss[param], yy, label=method)
+    plt.xlim(min(param_list), max(param_list))
+    plt.xlabel(param)
+    if xlogscale:
+        plt.xscale('log')
+    plt.xticks(param_list)
+    if diff:
+        plt.title(f'diff of loss from continuous evaluation')
+    else:
+        plt.title(f'{loss_name} obtained with 3 methods')
+    plt.legend()
+    plt.show()
 
+
+_ = procedure(T=10_000, L=200, p_task=0.6, verbose=True)
+# methods = ['dripp_cont', 'dripp_dis', 'torch', 'mix']
+methods = ['torch', 'mix']
 # %% Compute loss difference while varying p_task
 p_list = np.arange(0, 1.1, step=0.1)
 df_loss_p = Parallel(n_jobs=N_JOBS, verbose=1)(
     delayed(procedure)(T=10_000, L=200, p_task=this_p) for this_p in p_list)
 df_loss_p = pd.DataFrame(df_loss_p)
 
-for method in ['dripp_cont', 'dripp_dis', 'torch']:
-    plt.plot(df_loss_p['p_task'], df_loss_p[method]-df_loss_p['dripp_cont'],
-             label=method)
-plt.xlim(min(p_list), max(p_list))
-plt.xlabel('p_taks')
-plt.xticks(p_list)
-plt.title(f'{loss_name} obtained with 3 methods')
-plt.legend()
-plt.show()
+plot_losses(df_loss_p, methods, p_list, "p_task", diff=True)
 
 # %% Compute loss difference while varying L (4.4 min)
 L_list = [100, 200, 500, 1_000]
@@ -144,28 +174,14 @@ df_loss_L = Parallel(n_jobs=N_JOBS, verbose=1)(
     delayed(procedure)(T, this_L) for this_L in L_list)
 df_loss_L = pd.DataFrame(df_loss_L)
 
-for method in ['dripp_cont', 'dripp_dis', 'torch']:
-    plt.plot(df_loss_L['L'], df_loss_L[method]-df_loss_L['dripp_cont'], label=method)
-plt.xlim(min(L_list), max(L_list))
-plt.xlabel('L')
-plt.xticks(L_list)
-plt.title(f'{loss_name} obtained with 3 methods')
-plt.legend()
-plt.show()
+plot_losses(df_loss_L, methods, L_list, "L", diff=True, xlogscale=False)
 
 # %% Compute loss difference while varying T (2.6 s)
 T_list = [100, 500, 1_000, 5_000, 10_000, 50_000, 100_000]
 df_loss_T = Parallel(n_jobs=N_JOBS, verbose=1)(
-    delayed(procedure)(this_T, L, p_task=0.6) for this_T in T_list)
+    delayed(procedure)(T=this_T, L=200, p_task=0.6) for this_T in T_list)
 df_loss_T = pd.DataFrame(df_loss_T)
 
-for method in ['dripp_cont', 'dripp_dis', 'torch']:
-    plt.plot(df_loss_T['T'], df_loss_T[method]-df_loss_T['dripp_cont'], label=method)
-plt.xlim(min(T_list), max(T_list))
-plt.xlabel('T')
-plt.xscale('log')
-plt.xticks(T_list)
-plt.title(f'{loss_name} from dripp continuous')
-plt.legend()
-plt.show()
+plot_losses(df_loss_T, methods, T_list, "T", diff=True, xlogscale=True)
+
 # %%

raised_torch/model.py
@@ -32,7 +32,7 @@ class Model(nn.Module):
 
     def __init__(self, t, baseline, alpha, m, sigma, dt=1/100,
                  kernel_name='raised_cosine', loss_name='log-likelihood',
-                 lower=None, upper=None):
+                 lower=None, upper=None, driver=None):
 
         super().__init__()
 
@@ -58,15 +58,18 @@ class Model(nn.Module):
         self.lower = lower
         self.upper = upper
         self.register_buffer('t', t)
+        self.dt = dt
+        self.L = len(self.t)
+        self.loss_name = loss_name
+
+        self.n_driver_events = None
+        if driver is not None:
+            self.n_driver_events = [this_driver.sum().item() for this_driver in driver]
 
         # compute initial kernels
         self.kernels = compute_kernels(
             self.t, self.alpha, self.m, self.sigma, self.kernel_name,
-            self.lower, self.upper)
-
-        self.dt = dt
-        self.L = len(self.t)
-        self.loss_name = loss_name
+            self.lower, self.upper, self.dt)
 
     def forward(self, driver):
         """Function to be optimised (the intensity).,
@@ -84,6 +87,6 @@ class Model(nn.Module):
         # compute updated kernels
         self.kernels = compute_kernels(
             self.t, self.alpha, self.m, self.sigma, self.kernel_name,
-            self.lower, self.upper)
+            self.lower, self.upper, self.dt)
 
         return kernel_intensity(self.baseline, driver, self.kernels, self.L)

raised_torch/solver.py
@@ -183,7 +183,7 @@ def initialize(driver_tt, acti_tt, T, initializer='smart_start', lower=0,
     return init_params
 
 
-def compute_loss(loss_name, intensity, acti, dt, model=None):
+def compute_loss(loss_name, intensity, acti, dt, model=None, driver=None):
     """
 
     Parameters
@@ -197,13 +197,19 @@ def compute_loss(loss_name, intensity, acti, dt, model=None):
     """
     T = int(len(intensity) * dt)
     if loss_name == 'log-likelihood':
+        # first evaluate intensity integral
         if model is not None:
             nll = model.baseline * T
-            for this_alpha, this_n_driver_events in zip(model.alpha):
-                nll += this_alpha * this_n_driver_events
-            # XXX
+            n_driver_events = model.n_driver_events
+            if driver is not None:
+                n_driver_events = [this_driver.sum().item()
+                                   for this_driver in driver]
+            for this_alpha, this_n in zip(model.alpha, n_driver_events):
+                nll += this_alpha * this_n
+        else:
+            nll = intensity.sum() * dt
         # negative log-likelihood
-        return (intensity.sum() * dt - torch.log(intensity[np.where(acti)[0]]).sum())/T
+        return (nll - torch.log(intensity[np.where(acti)[0]]).sum())/T
     elif loss_name == 'MSE':
         return ((intensity ** 2).sum() * dt - 2 * (intensity[acti]).sum())/T
     else:
@@ -325,7 +331,8 @@ def training_loop(model, driver_tt, acti_tt, solver='RMSProp', step_size=1e-3,
             opt.zero_grad()
             intensity = model(driver_tt_train)
             v_loss = compute_loss(
-                model.loss_name, intensity, acti_tt_train, model.dt)
+                model.loss_name, intensity, acti_tt_train, model.dt, model,
+                driver_tt_train)
             if test:
                 intensity_test = model(driver_tt_test)
             if logging:
@@ -333,7 +340,7 @@ def training_loop(model, driver_tt, acti_tt, solver='RMSProp', step_size=1e-3,
                 if test:
                     hist[-1].update(loss_test=compute_loss(
                         model.loss_name, intensity_test,
-                        acti_tt_test, model.dt).item())
+                        acti_tt_test, model.dt, model, driver_tt_test).item())
             v_loss.backward()
             opt.step()
 
@@ -377,7 +384,8 @@ def training_loop(model, driver_tt, acti_tt, solver='RMSProp', step_size=1e-3,
                 'compute_time': end_time}
 
     # compute final loss
-    v_loss = compute_loss(model.loss_name, intensity, acti_tt_train, model.dt)
+    v_loss = compute_loss(model.loss_name, intensity, acti_tt_train, model.dt,
+    model, driver_tt_train)
     if test:
         intensity_test = model(driver_tt_test)
         res_dict.update(test_intensity=intensity_test.detach().cpu(),
@@ -396,7 +404,7 @@ def training_loop(model, driver_tt, acti_tt, solver='RMSProp', step_size=1e-3,
         if test:
             hist[-1].update(loss_test=compute_loss(
                 model.loss_name, intensity_test,
-                acti_tt_test, model.dt).item())
+                acti_tt_test, model.dt, model, driver_tt_test).item())
 
     res_dict['hist'] = hist
 

trunc_norm_kernel/model.py
@@ -106,7 +106,7 @@ class TruncNormKernel():
         """
 
         if (self.sfreq is None) or (not self.use_dis):
-            return self.eval(x, verbose=True)
+            return self.eval(x, verbose=False)
 
         x = np.asarray(x)
         x_idx = np.asarray(np.round((x - self.lower) * self.sfreq), dtype=int)